2ca9473bccd75dbaee2990164651afa2bb25059f
Alex Wright <anwturp@gmail.com>
2022-08-13 10:49:27 -0400

    Checked answers with group and changed 1a and 1b. Finished all parts of Q4 and moving onto Q5.

--- script/script_prescribers.sql
+++ script/script_prescribers.sql
@@ -1,22 +1,24 @@
 -- Q1a.
-SELECT p1.npi, p2.total_claim_count
+SELECT p1.npi, SUM(p2.total_claim_count) as total
 FROM prescriber as p1
 LEFT JOIN prescription as p2 USING(npi)
 WHERE p2.total_claim_count IS NOT NULL
-ORDER BY p2.total_claim_count DESC;
--- Answer: Highest claim count is 4,538 .
+GROUP BY p1.npi
+ORDER BY total DESC;
+-- Answer: Highest claim count is 99,707 .
 
 -- Q1b.
 SELECT 
     p1.nppes_provider_last_org_name as last_name,
     p1.nppes_provider_first_name as first_name,
     p1.specialty_description as specialty,
-    p2.total_claim_count
+    SUM(p2.total_claim_count) as total
 FROM prescriber as p1
 LEFT JOIN prescription as p2 USING(npi)
 WHERE p2.total_claim_count IS NOT NULL
-ORDER BY p2.total_claim_count DESC;
--- Answer: David Coffey, specialty being family practice .
+GROUP BY last_name, first_name, specialty
+ORDER BY total DESC;
+-- Answer: Bruce Pendley, specialty being family practice .
 
 -- Q2a.
 SELECT DISTINCT p1.specialty_description AS specailty, SUM(p2.total_claim_count) AS total
@@ -72,3 +74,28 @@ WHERE p2.total_drug_cost IS NOT NULL
 GROUP BY name
 ORDER BY cost_per_day DESC;
 -- Answer: "C1 ESTERASE INHIBITOR", 3,495.22 .
+
+-- Q4a.
+SELECT drug_name, 
+CASE 
+    WHEN opioid_drug_flag = 'Y' THEN 'Opioid'
+    WHEN antibiotic_drug_flag = 'Y' THEN 'Antibiotic'
+    ELSE 'Neither' 
+END AS drug_type
+FROM drug
+ORDER BY drug_name ASC;
+-- Answer: It works .
+
+-- Q4b.
+SELECT
+CASE 
+    WHEN d1.opioid_drug_flag = 'Y' THEN 'Opioid'
+    WHEN d1.antibiotic_drug_flag = 'Y' THEN 'Antibiotic'
+    ELSE 'Neither' 
+END AS drug_type, SUM(p2.total_drug_cost) AS money
+FROM drug as d1
+LEFT JOIN prescription as p2 USING(drug_name)
+GROUP BY drug_type
+ORDER BY money DESC;
+-- Answer: Opioids - $105,080,626.37 > Antibiotics - $38,435,121.26
+
